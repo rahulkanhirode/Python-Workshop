02a74369454eab0118aed146b271543b36c83d75
rahul <rahulindia0001@gmail.com>
2018-10-10 10:14:27 +0530

    part1_hands_on_exercise

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print("hello world!!")
+n = -10
+if n < 5:
+	print("n is less than five")
+elif n == 5:
+     print("n is equal to five")
+else:
+     print("n is greater than five")
+
+
+     answer = input("What is the air-speed velocity of an unladen swallow? ")
+What is the air-speed velocity of an unladen swallow? Huh? I... I don't know that.
+answer
---
+++ test_hands_on_exercise.py
@@ -0,0 +1,88 @@
+n = -10
+if n < 5:
+	print("n is less than five")
+elif n == 5:
+     print("n is equal to five")
+else:
+     print("n is greater than five")
+
+
+
+
+
+n = 10
+if n < 5:
+	print("n is less than five")
+elif n == 5:
+     print("n is equal to five")
+else:
+     print("n is greater than five")
+
+
+     n = 5
+
+if n < 5:
+	print("n is less than five")
+elif n == 5:
+     print("n is equal to five")
+else:
+     print("n is greater than five")
+
+
+import math
+import random
+
+pi = math.pi
+print ("The Pi value is ", pi, "and the type is", type(pi))
+
+
+i = 50
+if i < 50:
+    print(" i is less than 50")
+elif i >50:
+    print("i is greater than 50")
+else:
+    print("i is equals to 50")
+
+
+
+i = random.randint(0, 100)
+if i < 50:
+    print("i is less than 50")
+elif i >50:
+    print("i is greater than 50")
+else:
+    print("i is equals to 50")
+
+
+
+picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
+if picked_fruit == 'orange':
+    print("The fruit picked is ", picked_fruit, " and its colour is orange")
+elif picked_fruit == 'strawberry':
+    print("The fruit picked is ", picked_fruit, " and its colour is red")
+elif picked_fruit == 'banana':
+    print("The fruit picked is ", picked_fruit, " and its colour is yellow")
+
+
+
+def mul_two_num (a,b):
+    c = a*b
+    return = c
+
+mul_two_num()
+    print c
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
